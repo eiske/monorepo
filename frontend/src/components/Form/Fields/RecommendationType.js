d8d47e0300e0588ef7b8e92a86c2ad907fa92f8a
eiske <hashimoto.eiske@gmail.com>
2025-08-25 21:32:06 -0300

    feat: add selectedValue prop to RecommendationType component

--- frontend/src/components/Form/Fields/RecommendationType.js
+++ frontend/src/components/Form/Fields/RecommendationType.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Checkbox from '../../shared/Checkbox';
 
-function RecommendationType({ onRecommendationTypeChange }) {
+function RecommendationType({ selectedValue = 'MultipleProducts', onRecommendationTypeChange }) {
   return (
     <div className="mb-4">
       <h2 className="text-lg font-bold mb-2">Tipo de Recomendação:</h2>
@@ -10,6 +10,7 @@ function RecommendationType({ onRecommendationTypeChange }) {
           type="radio"
           name="recommendationType"
           value="SingleProduct"
+          checked={selectedValue === 'SingleProduct'}
           onChange={() => onRecommendationTypeChange('SingleProduct')}
           className="mr-2"
         />
@@ -18,6 +19,7 @@ function RecommendationType({ onRecommendationTypeChange }) {
           type="radio"
           name="recommendationType"
           value="MultipleProducts"
+          checked={selectedValue === 'MultipleProducts'}
           onChange={() => onRecommendationTypeChange('MultipleProducts')}
           className="mr-2"
         />
